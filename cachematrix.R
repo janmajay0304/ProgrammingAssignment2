961efe01969b991e45482284cd100ca011f76378
janmajay0304 <janmajay_0304@hotmail.com>
2014-04-27 20:26:18 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Please make sure to load MASS package
+#Use the below command
+#library(MASS)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	
+	get <- function() x
+	setInverse <- function(inpParam) m <<- inpParam
+	getInverse <- function() m
+	list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+	
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix()){
+	m <- x$getInverse()
+	if(!is.null(m)){
+		message("Returning cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- ginv(data)       # Necessary to load MASS package to use this function
+	x$setInverse(m)
+	m
+	
 }
